f1f489774478fb7d88bb3bcf2fd3e19eb5d42d79
MarouenGh <marwen.ghroubi@esprit.tn>
2022-02-23 12:04:28 +0100

    intial commit

---
+++ src/main/java/com/sg/testkata/TestKataApplication.java
@@ -0,0 +1,13 @@
+package com.sg.testkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestKataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sg/testkata/TestKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.testkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
